82a52a05cb56402becc72745692202f9c9e81256
Jeet Hore <jeethore2005@gmail.com>
2025-10-12 12:09:59 +0530

    Restructuring Listings using Express Router concept

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const {listingSchema, reviewSchema} = require("./schema.js");
 
 const Review=require("./models/review.js");
 
+const listing=require("./routes/listing.js");
+
 main().then(()=>{
     console.log("Connected to DB")
 }).catch(err=>{
@@ -32,15 +34,7 @@ app.get("/",(req,res)=>{
     res.send("Hi, I am root");
 });
 
-const validateListing=(req,res,next)=>{
-    let {error}=listingSchema.validate(req.body);
-   
-    if(error){
-        throw new ExpressError(400,error);
-    }else{
-        next();
-    }
-}
+
 
 const validateReview=(req,res,next)=>{
     let {error}=reviewSchema.validate(req.body);
@@ -52,68 +46,7 @@ const validateReview=(req,res,next)=>{
     }
 }
 
-
-
-//index route
-
-app.get("/listings",wrapAsync(async(req,res)=>{
-    const allListings=await Listing.find({});
-        console.log(allListings);
-        res.render("listings/index.ejs",{allListings});
-}));
-
-//new route
-app.get("/listings/new", (req,res)=>{
-    res.render("listings/new.ejs");
-});
-     
-
-//show route
-app.get("/listings/:id",wrapAsync(async(req,res)=>{
-    let {id}=req.params; 
-    const listing=await Listing.findById(id).populate("reviews");
-    res.render("listings/show.ejs",{listing});
-}));
-
-//create route
-app.post("/listings",validateListing,wrapAsync( async(req,res,next)=>{
-    // if(!req.body.listing){
-    //     throw new ExpressError(404,"send valid data for listing");
-    // }
-
-    const newListing=new Listing(req.body.listing);
-    await newListing.save();
-    res.redirect("/listings");
-})
-);
-
-//edit route
-
-app.get("/listings/:id/edit",wrapAsync(async(req,res)=>{
-    let {id}=req.params; 
-    const listing=await Listing.findById(id);
-    res.render("listings/edit.ejs",{listing});
-
-}));
-
-//update route
-
-app.put("/listings/:id",validateListing, wrapAsync(async(req,res)=>{
-    if(!req.body.listing){
-        throw new ExpressError(404,"send valid data for listing");
-    }
-    let {id}=req.params; 
-    await Listing.findByIdAndUpdate(id,{...req.body.listing});
-    res.redirect("/listings");
-}));
-
-// delete route
-
-app.delete("/listings/:id", wrapAsync(async(req,res)=>{
-    let {id}=req.params; 
-    await Listing.findByIdAndDelete(id);
-    res.redirect("/listings");
-}));
+app.use("/listings",listing)
 
 //Reviews 
 //Post route
