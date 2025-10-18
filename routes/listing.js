0322ba2e272c79c3d13ce2b6a22080f558f66e91
Jeet Hore <jeethore2005@gmail.com>
2025-10-18 10:33:36 +0530

    connecting to login route

--- routes/listing.js
+++ routes/listing.js
@@ -4,6 +4,7 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const ExpressError = require("../utils/ExpressError.js");
 const {listingSchema} = require("../schema.js");
 const Listing=require("../models/listing.js");
+const {isLoggedIn}=require("../middleware.js")
 
 const validateListing=(req,res,next)=>{
     let {error}=listingSchema.validate(req.body);
@@ -24,7 +25,7 @@ router.get("/",wrapAsync(async(req,res)=>{
 }));
 
 //new route
-router.get("/new", (req,res)=>{
+router.get("/new", isLoggedIn,(req,res)=>{
     res.render("listings/new.ejs");
 });
      
@@ -41,7 +42,7 @@ router.get("/:id",wrapAsync(async(req,res)=>{
 }));
 
 //create route
-router.post("/",validateListing,wrapAsync( async(req,res,next)=>{
+router.post("/", isLoggedIn,validateListing,wrapAsync( async(req,res,next)=>{
     // if(!req.body.listing){
     //     throw new ExpressError(404,"send valid data for listing");
     // }
@@ -55,7 +56,7 @@ router.post("/",validateListing,wrapAsync( async(req,res,next)=>{
 
 //edit route
 
-router.get("/:id/edit",wrapAsync(async(req,res)=>{
+router.get("/:id/edit", isLoggedIn,wrapAsync(async(req,res)=>{
     let {id}=req.params; 
     const listing=await Listing.findById(id);
     if(!listing){
@@ -68,7 +69,7 @@ router.get("/:id/edit",wrapAsync(async(req,res)=>{
 
 //update route
 
-router.put("/:id",validateListing, wrapAsync(async(req,res)=>{
+router.put("/:id", isLoggedIn,validateListing, wrapAsync(async(req,res)=>{
     if(!req.body.listing){
         throw new ExpressError(404,"send valid data for listing");
     }
@@ -80,7 +81,7 @@ router.put("/:id",validateListing, wrapAsync(async(req,res)=>{
 
 // delete route
 
-router.delete("/:id", wrapAsync(async(req,res)=>{
+router.delete("/:id", isLoggedIn, wrapAsync(async(req,res)=>{
     let {id}=req.params; 
     await Listing.findByIdAndDelete(id);
     req.flash("success","Listing Deleted ! ");
