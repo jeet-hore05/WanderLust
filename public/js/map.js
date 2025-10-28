340f5eaa870a975545533cc974f04cc42fe453b8
Jeet Hore <jeethore2005@gmail.com>
2025-10-28 13:21:16 +0530

    map marker & popUp

--- public/js/map.js
+++ public/js/map.js
@@ -2,10 +2,9 @@
     mapboxgl.accessToken = mapToken;
     const map = new mapboxgl.Map({
         container: 'map',
-        style: 'mapbox://styles/mapbox/standard', // Use the standard style for the map
-        projection: 'globe', // display the map as a globe
-        zoom: 5, // initial zoom level, 0 is the world view, higher values zoom in
-        center: [77, 29] // center the map on this longitude and latitude
+        style: 'mapbox://styles/mapbox/streets-v12', // Use the style for the map
+        zoom: 9, // initial zoom level, 0 is the world view, higher values zoom in
+        center: coordinates // center the map on this longitude and latitude
     });
 
     map.addControl(new mapboxgl.NavigationControl());
@@ -14,4 +13,14 @@
     map.on('style.load', () => {
         map.setFog({}); // Set the default atmosphere style
     });
+
+
+// create a marker at a coordinate
+const marker = new mapboxgl.Marker({ color: "red" })
+  .setLngLat(coordinates)
+  .setPopup(
+    new mapboxgl.Popup({ offset: 25 })
+      .setHTML("<p>Exact location provided after booking<p>")
+  )
+  .addTo(map);
 
